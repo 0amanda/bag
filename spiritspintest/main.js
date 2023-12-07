66d03ed67e0c0afe68ebd0d23e2caa4cc549f2f2
0amanda <amand20201@gmail.com>
2023-12-07 13:20:16 -0600

    added sprite spin test

--- spiritspintest/main.js
+++ spiritspintest/main.js
@@ -1,7 +1,6 @@
 
 $(function() {
-    $('#spritespin').spritespin({
-      source: [
+  var frames = [
     '../../risobagframes/frame_01.jpg',
     '../../risobagframes/frame_02.jpg',
     '../../risobagframes/frame_03.jpg',
@@ -13,13 +12,46 @@ $(function() {
     '../../risobagframes/frame_09.jpg',
     '../../risobagframes/frame_10.jpg',
     '../../risobagframes/frame_11.jpg',
-      ],
-      width: 600,
-      height: 500,
-      sense: -1
+    '../../risobagframes/frame_12.jpg',
+    '../../risobagframes/frame_13.jpg',
+    '../../risobagframes/frame_14.jpg',
+    '../../risobagframes/frame_15.jpg',
+    '../../risobagframes/frame_16.jpg',
+    '../../risobagframes/frame_17.jpg',
+    '../../risobagframes/frame_18.jpg',
+    '../../risobagframes/frame_19.jpg',
+    '../../risobagframes/frame_20.jpg',
+    '../../risobagframes/frame_21.jpg',
+    '../../risobagframes/frame_22.jpg',
+    '../../risobagframes/frame_23.jpg',
+    '../../risobagframes/frame_24.jpg',
+    '../../risobagframes/frame_25.jpg',
+    '../../risobagframes/frame_26.jpg',
+  ];
 
-    
+    var index = 9;
+
+    $("#mySpriteSpin").spritespin({
+      source: frames,
+      width : 500,
+      height : 400,
+      frame : index,
+      frames : 26,
+      animate   : false,
+      loop      : false,
+      loopFrame : 27,
+      wrap  :false,
+      frameWrap : false,
+      frameTime : 40,
+      enableCanvas : false,
+      currentFrame: function(){
+        // console.log(this.data.frame);
+        return this.data.frame;
+      },
     });
+  
+    // console.log(frames);
+
 })
 
         
